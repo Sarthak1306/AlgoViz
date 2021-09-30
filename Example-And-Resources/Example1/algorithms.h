8d4cd9a12de52d922159cb7d2abca041e93942da
Pranit5895 <guptapranit9@gmail.com>
2021-10-01 00:05:57 +0530

    Example Code 1

---
+++ Example-And-Resources/Example1/algorithms.h
@@ -0,0 +1,11 @@
+#define _ALGORITHMS_H
+
+#include "SDL2/SDL.h"
+
+void render(SDL_Renderer *, int *);
+void swap(int *, int *);
+int partition(int *, int, int);
+void quickSort(SDL_Renderer *, int *, int, int);
+void stoogeSort(SDL_Renderer *, int *, int , int);
+
+/* #endif  _ALGORITHMS_H */
