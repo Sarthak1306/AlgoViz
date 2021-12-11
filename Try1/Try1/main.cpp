e9c45f0fb8c36267c0e71381865c7a0cf3c0696d
Sarthak1306 <sarthak13062001@gmail.com>
2021-12-11 14:36:08 +0530

    Updated Sorting Algorithm

--- Try1/Try1/main.cpp
+++ Try1/Try1/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Network.hpp>
+//#include <SFML/Graphics.hpp>
+//#include <SFML/Audio.hpp>
+//#include <SFML/System.hpp>
+//#include <SFML/Window.hpp>
+//#include <SFML/Network.hpp>
 #include <GLUT/GLUT.h>
 #include <stdio.h>
 #include <math.h>
@@ -13,6 +13,7 @@
 #ifdef __APPLE__
 #else
 #endif
+
 
 #define MAX 10
 using namespace std;
@@ -32,6 +33,298 @@ const int NODES = 5 ; /* the number of nodes */
 int EDGES=0;            /* the number of edges */
 Edge edges[32];       /* large enough for n <= 2^NODES=32 */
 int d[32];            /* d[i] is the minimum distance from source node s to node i */
+
+
+
+// ---------- New Insertion Code -------------
+
+int k=0;
+typedef struct circle
+{
+    GLfloat x;    //x axis of center
+    GLfloat y;    //y axis of center
+    GLfloat r;    // radius
+}circle;
+
+circle c[MAX];    //array of circles to store the center and radius of each circle
+int arraySorting[MAX];        // int array for sorting algo
+int initialSorting[MAX] = {10,4,8,6,3,7,9,1,5,2};    //int array to restore random values after sorting
+GLfloat initial_x1,initial_x2;    //to set the destiniation for swapping
+
+int global_i = 0, global_j = 0;    //i and j values for bubble-sort
+int swapping = 0;    //flag to check if circle are being swapped
+int sorting = 0;    //start sorting only after user input
+
+void initialise()
+{
+    global_i = global_j = swapping = 0; //reset all globals
+    for (int i=0;i<MAX;i++)
+    {
+        a[i] = initialSorting[i];    //if a[] is sorted restore from initial[]
+        c[i].r = a[i]*4.0;    //4 because to fit 10 circles in screen
+        c[i].y = 300.0;        //vertical center of window
+        if (i == 0)
+            c[i].x = 45.0;    // first circle starts from 45 offset
+        else
+            c[i].x = c[i-1].x + 90.0;//(c[i-1].r+c[i].r+10.0); //distance between circles = sum of 2 max readii
+        printf("%f - %f - %f\n",c[i].x,c[i].y,c[i].r); //for testing purpose don worry
+    }
+}
+
+//func to display text on screen char by char
+void bitmap_output(int x, int y, char *string, void *font)
+{
+  int len, i;
+
+  glRasterPos2f(x, y);
+  len = (int) strlen(string);
+  for (i = 0; i < len; i++) {
+    glutBitmapCharacter(font, string[i]);
+  }
+}
+
+//function to integer to string
+void int_str(int rad,char r[])
+{
+    switch(rad)
+    {
+        case 1 : strcpy(r, "1"); break;
+        case 2 : strcpy(r, "2"); break;
+        case 3 : strcpy(r, "3"); break;
+        case 4 : strcpy(r, "4"); break;
+        case 5 : strcpy(r, "5"); break;
+        case 6 : strcpy(r, "6"); break;
+        case 7 : strcpy(r, "7"); break;
+        case 8 : strcpy(r, "8"); break;
+        case 9 : strcpy(r, "9"); break;
+        case 10 : strcpy(r, "10"); break;
+    }
+    
+}
+
+//draw circle by drawing consecutive triangle fans
+void circle_drawSorting(circle c)
+{
+    float i;
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(c.x, c.y);    //center of circle
+    for (i=0;i<360;i+=1)
+        glVertex2f(c.x + sin(i) * c.r, c.y + cos(i) * c.r);
+    glEnd();
+
+    //display the value of circle below
+    int x = c.x-2;
+    int y = c.y-(c.r+10);
+    int rad = c.r / 4;
+    char r[3] = "";
+    int_str(rad,r);
+    glColor3f(0.0,0.0,0.0);
+    bitmap_output(x, y, r, GLUT_BITMAP_TIMES_ROMAN_10);
+    
+}
+
+// swaps circles cc1 and cc2 by changing their centers
+void swap_circles(circle *cc1,circle *cc2)
+{
+    if (swapping == 0)    //if circles are not being swapped set destination for each circles
+    {
+        initial_x1 = cc1->x; //center of circle in left
+        initial_x2 = cc2->x; //center of circle in right
+        swapping = 1;    //means cicle are being swapped
+        printf("%f - %f\n",cc1->r,cc2->r);
+        
+    }
+
+    if (initial_x1 <= cc2->x)    //decrease the center of circle in right till its > center of left circle
+        cc2->x -= 1.0;        //speed of animation
+
+    if (initial_x2 >= cc1->x)//increase the center of circle in left till its < center of right circle
+        cc1->x += 1.0;
+
+    printf("one %f - %f\n",initial_x1,cc2->x);
+    printf("two %f - %f\n",initial_x2,cc1->x);
+
+    // if difference between destination and center < 0.3 then cicles are swapped
+    if (abs(initial_x1-cc2->x) < 0.3 && abs(initial_x2-cc1->x) < 0.3)  //set this to speed of animation
+    {
+        swapping = 0;
+    
+        int temp = cc1->x;
+        cc1->x = cc2->x;
+        cc2->x = temp;
+
+        temp = cc1->y;
+        cc1->y = cc2->y;
+        cc2->y = temp;
+
+        temp = cc1->r;
+        cc1->r = cc2->r;
+        cc2->r = temp;
+    }
+    
+}
+
+
+void sort()    //bubble sort algo
+{
+    if (!swapping) //if not in process of swappin 2 circles only then get 2 new circles to swap
+    {
+        while (global_i < 10)
+        {
+            global_j = global_i;
+            while (global_j < 9)
+            {
+                if (a[global_j] > a[global_j+1])
+                {
+                    printf("%d %d\n",global_j,a[global_j]);
+                    int temp = a[global_j];
+                    a[global_j] = a[global_j+1];
+                    a[global_j+1] = temp;
+                    goto SWAP;
+                    
+                }
+                global_j ++;
+            }
+            global_i ++;
+        }
+    }
+SWAP:
+    printf("swapping --> %d - %d\n",global_j,global_j+1);
+    bitmap_output(10, 375, "Swapping ->",GLUT_BITMAP_9_BY_15);
+    
+    char r[3]="";
+    int_str(a[global_j],r);
+    bitmap_output(150, 375, r,GLUT_BITMAP_9_BY_15);
+    int_str(a[global_j+1],r);
+    bitmap_output(175, 375, r,GLUT_BITMAP_9_BY_15);
+
+    swap_circles(&c[global_j],&c[global_j+1]);
+}
+
+void display_text()
+{
+    bitmap_output(200, 565, "DYNAMIC SORTING ALGORITHM VISUALISER",GLUT_BITMAP_TIMES_ROMAN_24);//title larger font
+    glBegin(GL_LINE_LOOP); //to underline the title
+        glVertex2f(180, 560);
+        glVertex2f(750, 560);
+    glEnd();
+    
+    //other text small font
+  
+    
+    if (sorting == 0)//if not sorting display menu
+    {
+        bitmap_output(10, 455, "MENU",GLUT_BITMAP_9_BY_15);
+        bitmap_output(10, 435, "Press S to start sorting",GLUT_BITMAP_9_BY_15);
+        bitmap_output(10, 415, "Press R to randomize",GLUT_BITMAP_9_BY_15);
+        bitmap_output(10, 395, "Esc to Exit",GLUT_BITMAP_9_BY_15);
+    }
+    else if (sorting == 1)
+    {
+        glColor3f(0.5,0.5,0.5);
+        bitmap_output(10, 455, "Sorting in progress...",GLUT_BITMAP_9_BY_15);
+        bitmap_output(10, 435, "Please do not quit",GLUT_BITMAP_9_BY_15);
+        glColor3f(0.0,0.0,0.0);
+    }
+    
+}
+
+void front()
+{
+    glColor3f(0.0,0.0,1.0);
+    bitmap_output(390, 565, "WELCOME!",GLUT_BITMAP_TIMES_ROMAN_24);
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(388, 560);
+        glVertex2f(524, 560);
+    glEnd();
+    bitmap_output(440, 535, "TO",GLUT_BITMAP_TIMES_ROMAN_24);
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(438, 531);
+        glVertex2f(475, 531);
+    glEnd();
+
+    bitmap_output(200, 495, "DYNAMIC SORTING ALGORITHM VISUALISER",GLUT_BITMAP_TIMES_ROMAN_24);
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(198, 490);
+        glVertex2f(731, 490);
+    glEnd();
+    glColor3f(1.0,0.0,0.0);
+    glBegin(GL_QUADS);
+          glVertex2f(594,120.0);glVertex2f(594,145);glVertex2f(836,145);glVertex2f(836,120.0);
+    glEnd();
+    glColor3f(0.0,1.0,0.0);
+    bitmap_output(600, 125, "Press Enter to continue.......",GLUT_BITMAP_HELVETICA_18);
+        
+}
+
+void displaySorting(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0,0.0,0.0);
+
+    if (k==0)
+    front();
+    else
+    {
+    
+    display_text();
+    
+    glPointSize(2.0);
+    for (int i=0;i<MAX;i++)
+    {
+        glColor3f(1.0,0.0,0.0);
+        circle_drawSorting(c[i]);
+    }
+    if (global_j+1 < MAX && sorting == 1) // call sort only on key press
+        sort();
+    else
+        sorting = 0;
+    }
+    
+    glFlush();
+    glutSwapBuffers();
+
+}
+
+void keyboardSorting (unsigned char key, int x, int y)
+{
+    if(key==13)
+        k=1;
+    if (k==1)
+    {
+    switch (key)
+    {
+        case 27 : exit (0); //27 is the ascii code for the ESC key
+        case 's' : sorting = 1; break;
+        case 'r' : initialise(); break;
+    }
+    }
+}
+
+void reshape(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if(w<=h)
+        glOrtho(-2.0, 2.0, -2.0 * (GLfloat) h/ (GLfloat) w, 2.0* (GLfloat) h / (GLfloat) w, -10.0, 10.0);
+    else
+        glOrtho(-2.0 * (GLfloat) w/ (GLfloat) h, 2.0* (GLfloat) w / (GLfloat) h, -2.0, 2.0, -10.0, 10.0);
+
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void initSorting(void)
+{
+        glClearColor(1.0,1.0,1.0,0.0);
+        glMatrixMode(GL_PROJECTION);
+        gluOrtho2D(0.0,900.0,0.0,600.0);
+}
+
+// ----------- Sorting Algo Finish -----------
+
+
+// -------------- Djikstra's Above -------------
 
 
 //prototype of dijkstra
@@ -205,6 +498,11 @@ void drawCircle(float cx, float cy, float r, float num_segments, char colorChara
     glFlush();
 
 }
+
+
+// -------------- Djikstra's Above -------------
+
+
 
 //draw square
 void drawSquare(int x, int y)
@@ -469,22 +767,7 @@ void beginDijkstra(unsigned char key, int x, int y)
 
 
 
-class Column{
-public:
-    float width;
-    float length;
-    float x;
-    float y;
-    float out;
-    sf::Color color = sf::Color::White;
-    sf::RectangleShape Draw() {
-        sf::RectangleShape col(sf::Vector2f(width, length));
-        col.setPosition(sf::Vector2f(x, y));
-        col.setOutlineColor(sf::Color::Green);
-        col.setFillColor(color);
-        return col;
-    }
-};
+
 
 
 //FUNCTION TO SELECT BITMAP FONT
@@ -1362,94 +1645,19 @@ int main(int argc, char* argv[] ) {
     switch(opt){
         case 1:
         {
-            sf::RenderWindow window(sf::VideoMode(800, 600), "Insertion Sort Algorithm");
-            int numbers[] = { 66, 83, 343, 111, 500, 182, 46, 370, 480, 527, 266, 167, 163, 551, 462, 101, 143, 549, 82, 100, 69, 482, 491, 388, 588, 424, 138, 564, 519, 504, 52, 97, 419, 171, 275, 48, 247, 23, 441, 98, 240, 420, 203, 582, 365, 415, 267, 301, 512, 210, 16, 278, 463, 139, 392, 581, 338, 74, 282, 211, 296, 518, 468, 54, 204, 363, 375, 33, 130, 99, 31, 59, 575, 37, 361, 39, 312, 434, 200, 106, 389, 221, 220, 255, 196, 68, 474, 292, 231, 513, 369, 283, 201, 445, 449, 194, 251, 294, 206, 274 };
-//            cout<<"Enter the Array"<<endl;
-//            for(int num = 0; num < 100; num++)
-//                cin>>numbers[num];
-            int len = std::end(numbers) - std::begin(numbers);
-            int height = 1;
-            float unit = 2000 / len;
-            sf::Time time = sf::milliseconds(1000);
-            bool done = false;
-            window.clear(sf::Color::Black);
-            sf::sleep(sf::seconds(5));
             
-            for (int k = 0;k < len;k++) {
-                Column col;
-                col.width = unit - 1;
-                col.length = -numbers[k] * height;
-                col.x = k * unit;
-                col.y = 600;
-                window.draw(col.Draw());
-                window.display();
-            }
-            while (window.isOpen()) {
-                sf::Event event;
-                while (window.pollEvent(event)) {
-                    if (event.type == sf::Event::Closed) {
-                        window.close();
-                    }
-                }
-                    
-                window.clear(sf::Color::Black);
-                
-                if (!done) {
-                    for (int i = 0;i < len;i++) {
-
-                        int j = i;
-                        
-                        while (j > 0 && numbers[j] < numbers[j - 1]) {
-                            
-                            sf::sleep(time);
-                            
-                            for (int k = 0;k < len;k++) {
-                                if (k != j) {
-                                    Column col;
-                                    col.width = unit - 1;
-                                    col.length = -numbers[k] * height;
-                                    col.x = k * unit;
-                                    col.y = 600;
-                                    window.draw(col.Draw());
-                                    //window.display();
-                                }
-                            }
-                            Column col;
-                            col.width = unit - 1;
-                            col.length = -numbers[j] * height;
-                            col.x = j * unit;
-                            col.y = 600;
-                            col.color = sf::Color::Red;
-                            window.draw(col.Draw());
-                            window.display();
-                            
-                            window.clear(sf::Color::Black);
-                            int a = numbers[j];
-                            numbers[j] = numbers[j - 1];
-                            numbers[j - 1] = a;
-                            j--;
-
-                        }
-                        
-                        if (i == len - 1) {
-                            done = true;
-                        }
-                    }
-                }
-                if (done) {
-                    for (int k = 0;k < len;k++) {
-                        Column col;
-                        col.width = unit - 1;
-                        col.length = -numbers[k] * height;
-                        col.x = k * unit;
-                        col.y = 600;
-                        col.color = sf::Color::Green;
-                        window.draw(col.Draw());
-                    }
-                }
-
-                window.display();
-            }
+            glutInit(&argc,argv);
+                    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+                    glutInitWindowPosition(50,50);
+                    glutInitWindowSize(900,600);
+                    glutCreateWindow("Dynamic Sorting Visualiser");
+                    initSorting();
+                    initialise();
+                    glutDisplayFunc(displaySorting);
+                    glutIdleFunc (displaySorting);
+                    glutKeyboardFunc (keyboardSorting);
+                    glutMainLoop();
+         
             break;
     }
     
